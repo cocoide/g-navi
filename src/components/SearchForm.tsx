49340679075722a8b7bbb6dd54b2d588ffbdb533
{cocoide} <{cocoide.com@gmail.com}>
2022-11-09 03:02:45 +0900

    adjust the design

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,4 +1,4 @@
-import React, { FC, RefObject, SyntheticEvent, useRef } from 'react'
+import { FC, RefObject, SyntheticEvent, useRef } from 'react'
 import { useUserFormMutator } from '../infrastructure/UserFormState'
 import { useShopDataSWR } from '../infrastructure/UseShopDataSRW'
 import { fetcher } from '../libs/fetcher'
@@ -45,10 +45,16 @@ export const SearchForm: FC<SearchFormProps> = ({ userSetKeyword, fallbackData }
 
     return (
         <>
-            <form ref={formRef} onSubmit={handlerOnSubmitSearch} className="flex flex-col justify-center">
-                <input className="h-10 p-3 mx-10"
-                    type="search" name="seachWord" placeholder="キーワードを入力して下さい" />
-                <button>SEARCH</button>
+            <form ref={formRef} onSubmit={handlerOnSubmitSearch} className="flex flex-raw justify-center bg-slate-200 mx-20 drop-shadow-md">
+                <input className="w-full h-10 p-3 my-1 ml-8 bg-transparent 
+                text-center"
+                    type="search" name="seachWord" placeholder="キーワードを入力" />
+                <button className='m-1'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM10.5 7.5v6m3-3h-6" />
+                    </svg>
+
+                </button>
             </form>
         </>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,43 +18,57 @@ const Home: FC<Props> = ({ fallbackData }) => {
   const { data } = useShopDataSWR(userSetKeyword, fallbackData)
   return (
     <div>
-      <h1 className="text-left">東京グルメ検索(ホットペッパーAPI)</h1>
-      <SearchForm userSetKeyword={`${userSetKeyword}`} fallbackData={fallbackData} />
-      {data?.results ? (
-        data.results.shop.map((shopData: ShopObj) => {
 
-          return (
-            <div key={shopData.id} className="">
-              <p>
-                <span className=""
-                >掲載店名:</span>
-                <br />
-                {shopData.name}
-              </p>
-              <p>
-                <span className=""
-                >最寄駅名:</span>
-                <br />
-                {shopData.station_name}
-              </p>
-              <p>
-                <span className=""
-                >お店ジャンル:</span>
-                <br />
-                {shopData.genre.name}
-              </p>
-              <p>
-                <span className=""
-                >お店ジャンルキャッチ:</span>
-                <br />
-                {shopData.genre.catch}
-              </p>
-            </div>
-          )
-        })
-      ) : (
-        <p>Loading...</p>
-      )}
+      <div className="flex flex-row justify-center place-items-center">
+        <h1 className="text-3xl m-4 text-center">東京グルメ検索
+        </h1>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-8 h-8">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9zm3.75 11.625a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+        </svg>
+      </div>
+
+      <SearchForm userSetKeyword={`${userSetKeyword}`} fallbackData={fallbackData} />
+      <main>
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 m-3">
+          {data?.results ? (
+            data.results.shop.map((shopData: ShopObj) => {
+
+              return (
+                <div key={shopData.id} className="rounded-md 
+                bg-gradient-to-tr from-indigo-300 to-indigo-100
+                p-3 md drop-shadow-2xl">
+                  <p>
+                    <span className=""
+                    >掲載店名:</span>
+                    <br />
+                    {shopData.name}
+                  </p>
+                  <p>
+                    <span className=""
+                    >最寄駅名:</span>
+                    <br />
+                    {shopData.station_name}
+                  </p>
+                  <p>
+                    <span className=""
+                    >お店ジャンル:</span>
+                    <br />
+                    {shopData.genre.name}
+                  </p>
+                  <p>
+                    <span className=""
+                    >お店ジャンルキャッチ:</span>
+                    <br />
+                    {shopData.genre.catch}
+                  </p>
+                </div>
+              )
+            })
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+      </main>
     </div>
   )
 }
